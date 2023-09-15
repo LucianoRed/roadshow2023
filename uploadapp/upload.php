d982b4a701e8d0014f5bbd2574dbf8ccce89d56f
LucianoRed <lscorsin@redhat.com>
2023-09-15 14:44:02 -0300

    Fix size of the picture

--- uploadapp/upload.php
+++ uploadapp/upload.php
@@ -17,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
     }
 
     // Check file size
-    if ($_FILES["image"]["size"] > 500000) {
+    if ($_FILES["image"]["size"] > 5000000) {
         echo "Sorry, your file is too large.";
         $uploadOk = 0;
     }
