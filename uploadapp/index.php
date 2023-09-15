df23ac42b3e20c2962e031796460e8f266508d0f
Luciano Scorsin <34887634+LucianoRed@users.noreply.github.com>
2023-09-15 12:05:12 -0300

    Create index.php

---
+++ uploadapp/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Upload App</title>
+    <!-- Include Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>Image Upload</h1>
+        <form action="upload.php" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="image">Choose an image:</label>
+                <input type="file" name="image" id="image" class="form-control-file">
+            </div>
+            <button type="submit" class="btn btn-primary">Upload</button>
+        </form>
+    </div>
+</body>
+</html>
