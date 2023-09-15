29665668c0a09cbfefb3d85be063080d290e38a4
Luciano Scorsin <34887634+LucianoRed@users.noreply.github.com>
2023-09-15 12:20:52 -0300

    Update upload.php

--- uploadapp/upload.php
+++ uploadapp/upload.php
@@ -1,25 +1,17 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
     $targetDir = "uploads/";
-    $targetFile = $targetDir . basename($_FILES["image"]["name"]);
+    $sessionId = session_id(); // Get the current session ID
+    $targetFile = $targetDir . $sessionId . "_" . basename($_FILES["image"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
     // Check if image file is a actual image or fake image
-    if (isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["image"]["tmp_name"]);
-        if ($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
-    }
-
-    // Check if file already exists
-    if (file_exists($targetFile)) {
-        echo "Sorry, the file already exists.";
+    $check = getimagesize($_FILES["image"]["tmp_name"]);
+    if ($check === false) {
+        echo "File is not an image.";
         $uploadOk = 0;
     }
 
@@ -40,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Sorry, your file was not uploaded.";
     } else {
         if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
-            echo "The file " . basename($_FILES["image"]["name"]) . " has been uploaded.";
+            echo "The file " . basename($_FILES["image"]["name"]) . " has been uploaded as " . $sessionId . "_" . basename($_FILES["image"]["name"]) . ".";
         } else {
             echo "Sorry, there was an error uploading your file.";
         }
