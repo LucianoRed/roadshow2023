c4309ae6685e81c84b5dc2c098b6fe2b2a639f0e
LucianoRed <lscorsin@redhat.com>
2023-09-16 00:56:20 -0300

    Adicionado loading

--- uploadapp/index.php
+++ uploadapp/index.php
@@ -10,7 +10,7 @@
 <body>
     <div class="container mt-5">
         <h1>Image Upload</h1>
-        <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <form id="uploadForm" action="upload.php" method="POST" enctype="multipart/form-data">
             <div class="form-group">
                 <label for="image">Choose an image:</label>
                 <input type="file" name="image" id="image" class="form-control-file">
@@ -18,5 +18,65 @@
             <button type="submit" name="submit" class="btn btn-primary">Upload</button>
         </form>
     </div>
+
+    <!-- Modal for Upload Progress -->
+    <div id="uploadModal" class="modal" tabindex="-1" role="dialog" style="display: none;">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <p>Uploading...</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" id="uploadProgressBar" role="progressbar" style="width: 0%"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#uploadForm').submit(function() {
+                // Display the modal when the form is submitted
+                $('#uploadModal').modal('show');
+
+                // Show progress bar as the file uploads
+                let progressBar = $('#uploadProgressBar');
+                let formData = new FormData(this);
+
+                $.ajax({
+                    xhr: function() {
+                        let xhr = new window.XMLHttpRequest();
+                        xhr.upload.addEventListener("progress", function(evt) {
+                            if (evt.lengthComputable) {
+                                let percentComplete = (evt.loaded / evt.total) * 100;
+                                progressBar.width(percentComplete + '%');
+                            }
+                        }, false);
+                        return xhr;
+                    },
+                    url: $(this).attr('action'),
+                    type: 'POST',
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    success: function(response) {
+                        // Hide the modal and reset progress bar
+                        $('#uploadModal').modal('hide');
+                        progressBar.width('0%');
+
+                        // Handle the response from the server
+                        alert(response);
+                    },
+                    error: function() {
+                        // Handle the upload error
+                        alert('Upload failed. Please try again.');
+                    }
+                });
+
+                return false;
+            });
+        });
+    </script>
 </body>
 </html>
