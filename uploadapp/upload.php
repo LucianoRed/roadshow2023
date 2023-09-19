d1a7daf5ba0566e046402083574fc3b051abb148
LucianoRed <lscorsin@redhat.com>
2023-09-19 18:37:30 -0300

    Teste container imagem

--- uploadapp/upload.php
+++ uploadapp/upload.php
@@ -15,6 +15,17 @@ session_start();
         max-width: 100%;
         height: auto;
     }
+    .image-container {
+    max-width: 400px; /* Set the maximum width */
+    margin: 0 auto; /* Center the image container */
+}
+
+/* Style for the image itself */
+.image-container img {
+    width: 100%; /* Make the image take up 100% of the container's width */
+    height: auto; /* Maintain the aspect ratio */
+    display: block; /* Remove extra space below the image */
+}
 </style>
 </head>
 <body>
@@ -119,6 +130,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
     <img src="mostrafoto2.php?image_name=<?php echo $image_name_future;?>"  class="imagem-responsiva"><br>
 </div>
 <div>
+<div class="image-container">
+        <img src="mostrafoto2.php?image_name=<?php echo $image_name_future;?>" alt="Sua imagem">
+    </div>
 
     <?php echo $conteudo;?>
 </div>
