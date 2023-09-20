1a9640b2d053e547dfdfeb6ce1c9bdd8364d230a
LucianoRed <lscorsin@redhat.com>
2023-09-20 11:28:14 -0300

    added contagem

---
+++ uploadapp/contagem.php
@@ -0,0 +1,11 @@
+<?php
+$directory = 'uploads/'; // Replace with the path to your directory
+
+// Use scandir to list files in the directory
+$files = scandir($directory);
+
+// Count the number of files (excluding . and ..)
+$fileCount = (count($files) - 2)/2;
+
+echo $fileCount;
+?>
